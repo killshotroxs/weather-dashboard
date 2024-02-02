48b3a266e75402d7df64fcaf0bde6255dc0cf123
killshotroxs <krishnalpanara0066@gmail.com>
2024-02-02 21:50:22 +0530

    error catching updated

--- src/App.js
+++ src/App.js
@@ -11,24 +11,30 @@ function App() {
   const [isCelsius, setIsCelsius] = useState(true);
 
   const fetchWeatherData = async (city) => {
+    if (!city) {
+      alert("Please Enter a City Name");
+      return;
+    }
+  
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`
       );
       const data = await response.json();
-
+  
       if (data.cod === '404') {
+        alert("Please enter a valid city Name")
         throw new Error('City not found');
       }
-
+  
       setWeatherData(data);
       setCity(city); 
     } catch (error) {
-      alert("Please enter correct city name");
       console.error("Error fetching weather data:", error);
-      setWeatherData(null); // Clear weather data on error
+      setWeatherData(null);
     }
   };
+  
 
   const handleToggleUnit = (isCelsius) => {
     setIsCelsius(isCelsius);
